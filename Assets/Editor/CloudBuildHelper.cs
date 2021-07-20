43dd4fcf17fa858844057459657a7b1a9d52f4aa
Nagaoka Aya <sora_55x@yahoo.co.jp>
2021-07-20 16:58:25 +0900

    ビルド呼び出し前のクラス実装

---
+++ Assets/Editor/CloudBuildHelper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+public class CloudBuildHelper : MonoBehaviour
+{
+    public static void PreExport()
+    {
+        //PlayerSettings.bundleVersion = String.Format("1.0.{0}", manifest.GetValue("buildNumber", "unknown"));
+        Debug.LogWarning("@@@@ call CloudBuildHelper#PreExport");
+    }
+}
