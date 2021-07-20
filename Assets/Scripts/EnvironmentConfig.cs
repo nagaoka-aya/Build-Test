5ee823ea36499e2d03d2b403caa1bf237b4d550f
Nagaoka Aya <sora_55x@yahoo.co.jp>
2021-07-20 16:31:03 +0900

    new

---
+++ Assets/Scripts/EnvironmentConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentConfig : MonoBehaviour
+{
+    [SerializeField] EnvironmentType _EnvironmentType;
+    [SerializeField] List<Environment> _Environments;
+
+    private static EnvironmentConfig _SINGLETON;
+
+    private void Awake()
+    {
+        _SINGLETON = this;
+    }
+
+    static public Environment GetEnvironment()
+    {
+        return _SINGLETON.GetEnvironmentInner();
+    }
+
+    private Environment GetEnvironmentInner()
+    {
+        switch (_EnvironmentType)
+        {
+            case EnvironmentType.DEVELOP:
+                return _Environments[0];
+            case EnvironmentType.PRODUCT:
+                return _Environments[1];
+        }
+        throw new System.ArgumentException("参照不能");
+    }
+}
---
+++ Assets/Scripts/TextView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Environment environment = EnvironmentConfig.GetEnvironment();
+        GetComponent<Text>().text = environment.type;
+    }
+
+}
